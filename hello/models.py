2f7f66a6bddf56811143864fb505a06aaf851cdd
Roda <giovanna.roda@gmail.com>
2019-08-15 11:04:58 +0200

    conf

--- hello/models.py
+++ hello/models.py
@@ -3,3 +3,17 @@ from django.db import models
 # Create your models here.
 class Greeting(models.Model):
     when = models.DateTimeField("date created", auto_now_add=True)
+
+class Book(models.Model):
+    BOOK_TYPES = (
+        (HARDCOVER, 'Hardcover'),
+        (PAPERBACK, 'Paperback'),
+        (EBOOK, 'E-book'),
+    )
+    title = models.CharField(max_length=140)
+    author = models.CharField(max_length=30, blank=True)
+    publication_date = models.DateField(null=True)
+    pages = models.IntegerField(blank=True, null=True)
+    book_type = models.PositiveSmallIntegerField(choices=BOOK_TYPES)
+    abstract = models.CharField(max_length=2800)
+
