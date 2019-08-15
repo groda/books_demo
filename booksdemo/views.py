72b6f2309f3d9416e069c11b29d287d249fb3631
Roda <giovanna.roda@gmail.com>
2019-08-15 12:41:08 +0200

    bk view

--- booksdemo/views.py
+++ booksdemo/views.py
@@ -2,17 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import requests
 import os
-
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Greeting
+from .models import Book
 
 # Create your views here.
-###def index(request):
-###    # return HttpResponse('Hello from Python!')
-###    return render(request, "index.html")
-####def index(request):
-####    r = requests.get('http://httpbin.org/status/418')
-####    print(r.text)
-####    return HttpResponse('<pre>' + r.text + '</pre>')
 def index(request):
     times = int(os.environ.get('TIMES',3))
     name = os.environ.get('UNAME')
@@ -27,3 +23,24 @@ def db(request):
     greetings = Greeting.objects.all()
 
     return render(request, "db.html", {"greetings": greetings})
+
+
+class BookList(ListView):
+    model = Book
+
+class BookView(DetailView):
+    model = Book
+
+class BookCreate(CreateView):
+    model = Book
+    fields = ['title', 'author']
+    success_url = reverse_lazy('book_list')
+
+class BookUpdate(UpdateView):
+    model = Book
+    fields = ['title', 'author']
+    success_url = reverse_lazy('book_list')
+
+class BookDelete(DeleteView):
+    model = Book
+    success_url = reverse_lazy('book_list')
