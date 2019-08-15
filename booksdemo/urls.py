6921270f65a7a3212bd1d2c2ab246e6615c80963
Roda <giovanna.roda@gmail.com>
2019-08-15 13:18:56 +0200

    urls

--- booksdemo/urls.py
+++ booksdemo/urls.py
@@ -17,10 +17,10 @@ urlpatterns = [
     path("", booksdemo.views.index, name="index"),
     path("db/", booksdemo.views.db, name="db"),
     path("admin/", admin.site.urls),
-    url(r'^$', views.book_list, name='book_list'),
-    url(r'^(?P<pk>\d+)/delete/$', views.book_delete, name='book_delete'),
-    url(r'^(?P<pk>\d+)/update/$', views.book_update, name='book_update'),
-    url(r'^create/$', views.book_create, name='book_create'),
+    url(r'^$', booksdemo.views.book_list, name='book_list'),
+    url(r'^(?P<pk>\d+)/delete/$', booksdemo.views.book_delete, name='book_delete'),
+    url(r'^(?P<pk>\d+)/update/$', booksdemo.views.book_update, name='book_update'),
+    url(r'^create/$', booksdemo.views.book_create, name='book_create'),
     path('books/', include('books.urls')),
 ]
 
