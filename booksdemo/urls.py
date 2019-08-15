e9b8617d973bbc6562a3e348fbdd26b3462da6a1
Roda <giovanna.roda@gmail.com>
2019-08-15 13:10:39 +0200

    urls

--- booksdemo/urls.py
+++ booksdemo/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path, include
-
+from django.conf.urls import url
 from django.contrib import admin
-
 admin.autodiscover()
-
 import booksdemo.views
 
 # To add a new path, first import the app:
@@ -14,8 +12,16 @@ import booksdemo.views
 #
 # Learn more here: https://docs.djangoproject.com/en/2.1/topics/http/urls/
 
+
 urlpatterns = [
     path("", booksdemo.views.index, name="index"),
     path("db/", booksdemo.views.db, name="db"),
     path("admin/", admin.site.urls),
+    url(r'^$', views.book_list, name='book_list'),
+    url(r'^(?P<pk>\d+)/delete/$', views.book_delete, name='book_delete'),
+    url(r'^(?P<pk>\d+)/update/$', views.book_update, name='book_update'),
+    url(r'^create/$', views.book_create, name='book_create'),
+    path('books/', include('books.urls')),
 ]
+
+
